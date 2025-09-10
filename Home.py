73cae1ab1486c900dd489416ed6ce3ccde462fd6
Collins Mwiti <collinsmwiti98@gmail.com>
2025-09-10 10:25:44 +0300

    Change2

--- Home.py
+++ Home.py
@@ -23,4 +23,45 @@ Collinskimathimwiti@gmail.com|
 
     
 st.title("🌱 GeoAgri Dashboard")
-st.markdown("An app developed by for Soil Health and crop health Spatial Data Analysis.")   
+
+st.markdown(
+    """
+    The **GeoAgri Dashboard** is an interactive web application designed to support 
+    **soil health, crop health, and spatial data analysis** for farmers, agronomists, and researchers.  
+    It integrates [Streamlit](https://streamlit.io) with open-source geospatial libraries, including 
+    [Leafmap](https://leafmap.org), [Geemap](https://geemap.org), and [Folium](https://python-visualization.github.io/folium/).  
+
+    Key features include:  
+    - 🌍 NDVI/EVI vegetation analysis  
+    - 🌱 Soil nutrient dashboards (ISDA)
+    - 🌦️ Weather data visualization
+    - 🌾 Crop nutrient requirements  
+    - 🐛 Plant Disease Detection 
+    - 📊 Soil nutrient removal values  
+    - 🗺️ Field Boundary mapping 
+ 
+    """
+)
+
+
+st.subheader("Timelapse of Satellite Imagery")
+
+
+
+row1_col1, row1_col2 = st.columns(2)
+with row1_col1:
+    st.image("Data\spain.gif")
+
+with row1_col2:
+    st.image("Data\goes.gif")
+
+
+
+st.markdown(
+    """
+    <p style= font-size:18px;'>
+    <i>The data aims to empower users with actionable insights for sustainable agriculture and improved crop yields.</i>
+    </p>
+    """,
+    unsafe_allow_html=True
+)
